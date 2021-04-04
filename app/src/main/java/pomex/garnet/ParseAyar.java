6755fbacaf1a7d617e7df4d6310855492be881f3
a9T9a <71823597+a9T9a@users.noreply.github.com>
2021-04-04 16:18:42 +0300

    Update ParseAyar.java

--- app/src/main/java/pomex/garnet/ParseAyar.java
+++ app/src/main/java/pomex/garnet/ParseAyar.java
@@ -11,8 +11,8 @@ public class ParseAyar extends Application {
 
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("gUcsnX4jER2vV3Ch6rStdF5801Mha8HJLGD3HSfX")
-                .clientKey("PRX7AxCoBdT6oihJuBs4HSA6ZEC9EwVheFb7YutS")
+                .applicationId("---------------")
+                .clientKey("---------------")
                 .server("https://parseapi.back4app.com/")
                 .build());
     }
